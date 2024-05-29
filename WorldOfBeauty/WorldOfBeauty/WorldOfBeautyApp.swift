cf229d4efaf65b94b36940f117d53f0f55afaee0
Dhaval Barot <dhaval.barot@publicissapient.com>
2024-05-29 12:24:44 +0530

    Initial project

---
+++ WorldOfBeauty/WorldOfBeauty/WorldOfBeautyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldOfBeautyApp.swift
+//  WorldOfBeauty
+//
+//  Created by Dhaval Barot on 29/05/24.
+//
+
+import SwiftUI
+
+@main
+struct WorldOfBeautyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
